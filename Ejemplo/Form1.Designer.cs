8b04f2678eb5be0b70d41af37d36e2a88711e80d
unknown <lachavez201@gmail.com>
2020-09-28 16:54:35 -0500

    Este es mi primer COMMIT

---
+++ Ejemplo/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Ejemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtSeleccion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(69, 76);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 33);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(69, 41);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(59, 199);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(178, 20);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Numero Primo",
+            "Fibonacci",
+            "Factorial"});
+            this.comboBox1.Location = new System.Drawing.Point(184, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Selecciona Opcion";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(59, 238);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(146, 43);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "&S a l i r";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtSeleccion
+            // 
+            this.txtSeleccion.Location = new System.Drawing.Point(198, 76);
+            this.txtSeleccion.Name = "txtSeleccion";
+            this.txtSeleccion.Size = new System.Drawing.Size(107, 20);
+            this.txtSeleccion.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 302);
+            this.Controls.Add(this.txtSeleccion);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Programa de Ejemplos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtSeleccion;
+    }
+}
+
---
+++ Ejemplo/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int A;
+            int R;
+            int i;
+            int j;
+            txtSeleccion.Text = comboBox1.SelectedIndex.ToString();
+            switch (comboBox1.SelectedIndex)
+            {           
+                case -1: 
+                    // SELECCIONA OPCION
+                    txtSeleccion.Text = comboBox1.SelectedIndex.ToString();
+                    MessageBox.Show("Selecciona Opcion","Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    break;
+                case 0: 
+                    // OPCION NUMERO PRIMO
+                    //txtSeleccion.Text = comboBox1.SelectedIndex.ToString();
+                    //MessageBox.Show("Has Seleccionado la Opcion: " + comboBox1.SelectedItem.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtResultado.Clear();
+                    for ( i=2; i<=int.Parse(txtNumero1.Text);i++)
+                    {
+                        for (j=2;j<=int.Parse(txtNumero1.Text);j++)
+                        {
+                            if( i % j == 0)
+                            {
+                                txtResultado.Text=txtResultado.Text+j;
+                            }
+                        }
+                    }
+                    //txtResultado.Text = R;
+                    break;
+                default:
+                    break;
+            }
+        }
+            
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
